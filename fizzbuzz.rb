3c36c42c24dafc27386d9153173d3c080d7b515e
student online <eliel.badon@gmail.com>
2018-09-16 00:56:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if (number%5==0 && number%3==0)
-    "FizzBuzz" % 3 == 0 
-    "Fizz"
+    "FizzBuzz" 
+  
   elsif number % 5 == 0 
     "Buzz"
-  elsif (number%5==0 && number%3==0)
-    "FizzBuzz"
+  elsif number % 3 == 0 
+    "Fizz"
   end
 end
