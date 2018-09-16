9d58b14eea6ca7551068105c4c5f05e552cc086d
student online <eliel.badon@gmail.com>
2018-09-16 00:47:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  number = "Fizz"
+  if number % 3 == 0 
+    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  end
 end
